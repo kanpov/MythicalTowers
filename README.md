55413f286a5c67c44941213cc75a114ef49a8000
RedGrapefruit <karpovanton729@gmail.com>
2021-05-19 20:57:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+The mod is currently early work-in-progress.  
+Feel free to browse the code and contribute.  
+Expected release date: around 15th July
---
+++ src/main/java/com/redgrapefruit/mythicaltowers/common/MythicalTowers.java
@@ -0,0 +1,10 @@
+package com.redgrapefruit.mythicaltowers.common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MythicalTowers implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
