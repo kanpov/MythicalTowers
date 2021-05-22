2d20f3eed70297485e5288393a57048efbc5a2cd
RedGrapefruit <karpovanton729@gmail.com>
2021-05-22 11:44:04 +0300

    Added a sample green amulet

--- src/main/java/com/redgrapefruit/mythicaltowers/common/init/MythicalItems.java
+++ src/main/java/com/redgrapefruit/mythicaltowers/common/init/MythicalItems.java
@@ -1,7 +1,9 @@
 package com.redgrapefruit.mythicaltowers.common.init;
 
 import com.redgrapefruit.mythicaltowers.common.init.names.ItemNames;
+import com.redgrapefruit.mythicaltowers.common.item.AmuletItem;
 import com.redgrapefruit.mythicaltowers.common.item.OrbItem;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.util.registry.Registry;
 
@@ -15,9 +17,11 @@ public class MythicalItems {
     public static final OrbItem GREEN_ORB = new OrbItem(MythicalEffectConfigs.GREEN_ORB);
 
     // Amulets
+    public static final AmuletItem GREEN_AMULET = new AmuletItem(StatusEffects.JUMP_BOOST, 1);
 
     public static void init() {
         register(ItemNames.GREEN_ORB, GREEN_ORB);
+        register(ItemNames.GREEN_AMULET, GREEN_AMULET);
     }
 
     /**
--- src/main/java/com/redgrapefruit/mythicaltowers/common/init/names/ItemNames.java
+++ src/main/java/com/redgrapefruit/mythicaltowers/common/init/names/ItemNames.java
@@ -4,5 +4,9 @@ package com.redgrapefruit.mythicaltowers.common.init.names;
  * Names for the mod's items
  */
 public class ItemNames {
+    // Orbs
     public static final String GREEN_ORB = "green_orb";
+
+    // Amulets
+    public static final String GREEN_AMULET = "green_amulet";
 }
