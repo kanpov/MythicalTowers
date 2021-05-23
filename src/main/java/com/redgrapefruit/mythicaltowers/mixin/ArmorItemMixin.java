bfe7f3385a59111a177f99ff4cc8df38b8d5463e
RedGrapefruit <karpovanton729@gmail.com>
2021-05-23 10:59:15 +0300

    Added a mixin to apply armor knockback resistance

---
+++ src/main/java/com/redgrapefruit/mythicaltowers/mixin/ArmorItemMixin.java
@@ -0,0 +1,60 @@
+package com.redgrapefruit.mythicaltowers.mixin;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import com.redgrapefruit.mythicaltowers.common.init.MythicalArmorMaterials;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+/**
+ * This mixin applies knockback resistance to mod armor since Mojang decided to make knockback resistance immutable.<br>
+ * Inspired by Fabric Wiki page about adding armor to the game
+ */
+@Mixin(ArmorItem.class)
+public class ArmorItemMixin {
+    @Shadow @Final
+    private static UUID[] MODIFIERS;
+    @Shadow @Final @Mutable
+    private Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+    @Shadow @Final
+    protected float knockbackResistance;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void constructor(ArmorMaterial material, EquipmentSlot slot, Item.Settings settings, CallbackInfo info) {
+        // Get the UUID
+        UUID uuid = MODIFIERS[slot.getEntitySlotId()];
+
+        // Check if the ArmorMaterial is from the mod
+        if (material instanceof MythicalArmorMaterials) {
+            // Make a builder
+            ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+            // Add all attribute modifiers to the builder
+            attributeModifiers.forEach(builder::put);
+            // Add our knockback resistance
+            builder.put(
+                    EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE,
+                    new EntityAttributeModifier(
+                            "Armor knockback resistance",
+                            knockbackResistance,
+                            EntityAttributeModifier.Operation.ADDITION
+                    )
+            );
+            // Assign the attribute modifiers
+            attributeModifiers = builder.build();
+        }
+    }
+}
