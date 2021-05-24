277a491c38ca3d287b78a1764ed9464377032b8a
RedGrapefruit <karpovanton729@gmail.com>
2021-05-24 17:37:28 +0300

    Imported Container API from ImprovedFood

---
+++ src/main/java/com/redgrapefruit/mythicaltowers/api/ContainerBlock.java
@@ -0,0 +1,123 @@
+package com.redgrapefruit.mythicaltowers.api;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.*;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A container block synced with a {@link ContainerBlockEntity}.<br>
+ * Opens a {@link Screen} & a {@link ScreenHandler} when right-clicked.<br>
+ * Also acts as a directional block.<br><br>
+ * A part of RedCore.Container library bundled with this mod.
+ */
+public abstract class ContainerBlock extends BlockWithEntity {
+    protected DirectionProperty FACING; // Facing property
+
+    protected ContainerBlock(Settings settings) {
+        super(settings);
+        // Defaults facing property
+        setDefaultState(getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    /**
+     * Checks if given {@link BlockEntity} is instanceof current {@link BlockEntity}
+     *
+     * @param blockEntity Given {@link BlockEntity}
+     * @return Yes/No
+     */
+    protected abstract boolean checkBlockEntity(BlockEntity blockEntity);
+
+    /**
+     * Casts current {@link BlockEntity} to an inventory
+     *
+     * @param blockEntity Current block entity
+     * @return Casted to inventory
+     */
+    protected abstract Inventory castToInventory(BlockEntity blockEntity);
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        FACING = Properties.HORIZONTAL_FACING;
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return getDefaultState().with(FACING, context.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        // BlockWithEntity resets render type to INVISIBLE, so we deny it
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        // This operation is server-side
+        if (!world.isClient) {
+            // Create a screen handler factory and open the screen on the player's HUD
+            NamedScreenHandlerFactory screenHandlerFactory = state.createScreenHandlerFactory(world, pos);
+
+            if (screenHandlerFactory != null) {
+                player.openHandledScreen(screenHandlerFactory);
+            }
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        // Proceed if the block has changed
+        if (state.getBlock() != newState.getBlock()) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+
+            // If the block entity is current block entity, drop everything and update comparators
+            if (checkBlockEntity(blockEntity)) {
+                ItemScatterer.spawn(world, pos, castToInventory(blockEntity));
+                world.updateComparators(pos, this);
+            }
+
+            super.onStateReplaced(state, world, pos, newState, moved);
+        }
+    }
+
+    @Override
+    public boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+        return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
+    }
+}
---
+++ src/main/java/com/redgrapefruit/mythicaltowers/api/ContainerScreen.java
@@ -0,0 +1,70 @@
+package com.redgrapefruit.mythicaltowers.api;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+/**
+ * A container {@link Screen} displaying the GUI.<br>
+ * Can be overridden using built-in events to add custom GUI elements using vanilla {@link Screen} and {@link HandledScreen} features.<br><br>
+ * A part of RedCore.Container library bundled with this mod.
+ */
+public abstract class ContainerScreen extends HandledScreen<ScreenHandler> {
+    private final Identifier texture = getTexture();
+
+    protected ContainerScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+    }
+
+    /**
+     * Returns the {@link Screen} GUI texture
+     *
+     * @return GUI texture resource ID
+     */
+    protected abstract Identifier getTexture();
+
+    /**
+     * An event reserved for custom rendering and custom GUI elements
+     */
+    protected abstract void onRender(MatrixStack matrices);
+
+    @Override
+    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+        // Reset color
+        RenderSystem.color4f(1.0f, 1.0f, 1.0f, 1.0f);
+        // Bind texture
+        if (client != null) {
+            client.getTextureManager().bindTexture(texture);
+        } else {
+            // Removed temporarily until logging is implemented
+            //Logging.error("Couldn't bind texture; MinecraftClient instance is null");
+        }
+        // Calculate center position
+        int x = (width - backgroundWidth) / 2;
+        int y = (height - backgroundHeight) / 2;
+        // Render the texture
+        drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        // Render background and mouseover tooltip
+        renderBackground(matrices);
+        super.render(matrices, mouseX, mouseY, delta);
+        drawMouseoverTooltip(matrices, mouseX, mouseY);
+        // Fire event
+        onRender(matrices);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        // Center title
+        titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
+    }
+}
