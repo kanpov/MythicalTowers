9bf474c199b1eeba411d80a55f924d3ab17a5a32
RedGrapefruit <karpovanton729@gmail.com>
2021-05-24 20:04:17 +0300

    Setup a basic foundation for custom TNT

---
+++ src/main/java/com/redgrapefruit/mythicaltowers/api/tnt/AbstractCustomTntEntity.java
@@ -0,0 +1,29 @@
+package com.redgrapefruit.mythicaltowers.api.tnt;
+
+import com.redgrapefruit.mythicaltowers.common.util.TntEntityMixinAccess;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A custom {@link TntEntity} with the ability to customize the fuse timer and explosion power
+ */
+public abstract class AbstractCustomTntEntity extends TntEntity {
+    public AbstractCustomTntEntity(EntityType<? extends TntEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    public AbstractCustomTntEntity(World world, double x, double y, double z, @Nullable LivingEntity igniter) {
+        super(world, x, y, z, igniter);
+    }
+
+    protected void setFuseTimer(int fuse) {
+        setFuse(fuse);
+    }
+
+    protected void setExplosionPower(int explosionPower) {
+        ((TntEntityMixinAccess) this).setExplosionPower(explosionPower);
+    }
+}
