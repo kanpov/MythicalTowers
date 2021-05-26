9e32e2eb8d7b8569e7ec38dbdb515b501ab933fa
RedGrapefruit <karpovanton729@gmail.com>
2021-05-26 16:35:10 +0300

    Cleaned up TntEntityMixin a little bit

--- src/main/java/com/redgrapefruit/mythicaltowers/mixin/TntEntityMixin.java
+++ src/main/java/com/redgrapefruit/mythicaltowers/mixin/TntEntityMixin.java
@@ -8,12 +8,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 /**
- * This mixin allows to modify explosion power of TNT
+ * This mixin allows to modify explosion power of TNT.
  */
 @Mixin(TntEntity.class)
 public class TntEntityMixin implements TntEntityMixinAccess {
     @Unique
     private float explosionPower;
+    @Unique
+    private boolean isCustom = false;
 
     @ModifyVariable(method = "explode", at = @At("INVOKE_ASSIGN"))
     private float modifyExplosionPowerVariable(float originalExplosionPower) {
@@ -23,7 +25,7 @@ public class TntEntityMixin implements TntEntityMixinAccess {
     @Override
     public float getExplosionPower() {
         // Set explosion power to vanilla if not explicitly set by the mod
-        if (explosionPower == 0.0f) explosionPower = 4.0f;
+        if (!isCustom) explosionPower = 4.0f;
 
         return explosionPower;
     }
@@ -31,5 +33,6 @@ public class TntEntityMixin implements TntEntityMixinAccess {
     @Override
     public void setExplosionPower(float explosionPower) {
         this.explosionPower = explosionPower;
+        this.isCustom = true;
     }
 }
