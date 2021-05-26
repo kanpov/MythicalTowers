803e8154792ea168114faefa523482f1460c0d2d
RedGrapefruit <karpovanton729@gmail.com>
2021-05-26 17:29:44 +0300

    Replaced vanilla TntEntity usage with custom entities

--- src/main/java/com/redgrapefruit/mythicaltowers/common/block/trap/AbstractCustomTntBlock.java
+++ src/main/java/com/redgrapefruit/mythicaltowers/common/block/trap/AbstractCustomTntBlock.java
@@ -6,7 +6,9 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.TntBlock;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -24,4 +26,15 @@ public abstract class AbstractCustomTntBlock<TEntity extends AbstractCustomTntEn
 
     protected abstract TEntity createEntity(World world, double x, double y, double z, @Nullable LivingEntity igniter);
 
+    @Override
+    public void onDestroyedByExplosion(World world, BlockPos pos, Explosion explosion) {
+        // Server-side logic
+        if (world.isClient) return;
+
+        // Create the entity and set its fuse value
+        TEntity entity = createEntity(world, (double) pos.getX() + 0.5D, pos.getY(), pos.getZ(), explosion.getCausingEntity());
+        entity.setFuse((short) (world.random.nextInt(entity.getFuseTimer() / 4) + entity.getFuseTimer() / 8));
+        // Spawn the entity
+        world.spawnEntity(entity);
+    }
 }
