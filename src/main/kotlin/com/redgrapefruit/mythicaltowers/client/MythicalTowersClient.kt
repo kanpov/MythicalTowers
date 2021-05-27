cf7f541af313f4f28452005d6e7f16f2545dd15d
RedGrapefruit <karpovanton729@gmail.com>
2021-05-27 08:51:14 +0300

    We are going Kotlin!!!

---
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/client/MythicalTowersClient.kt
@@ -0,0 +1,10 @@
+package com.redgrapefruit.mythicaltowers.client
+
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+
+@Environment(EnvType.CLIENT)
+class MythicalTowersClient : ClientModInitializer {
+    override fun onInitializeClient() {}
+}
---
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/common/MythicalTowers.kt
@@ -0,0 +1,34 @@
+package com.redgrapefruit.mythicaltowers.common
+
+import com.redgrapefruit.mythicaltowers.common.init.MythicalBlocks
+import com.redgrapefruit.mythicaltowers.common.init.MythicalItemGroups
+import com.redgrapefruit.mythicaltowers.common.init.MythicalItems
+import net.fabricmc.api.ModInitializer
+import net.minecraft.util.Identifier
+import java.util.Random
+
+class MythicalTowers : ModInitializer {
+    override fun onInitialize() {
+        MythicalItemGroups.init()
+        MythicalItems.init()
+        MythicalBlocks.init()
+    }
+
+    companion object {
+        private const val MOD_ID = "mythicaltowers"
+
+        @JvmField
+        val RANDOM = Random()
+
+        /**
+         * Returns the [Identifier] of given name
+         *
+         * @param name Name
+         * @return Generated [Identifier]
+         */
+        @JvmStatic
+        fun idOf(name: String?): Identifier {
+            return Identifier(MOD_ID, name)
+        }
+    }
+}
