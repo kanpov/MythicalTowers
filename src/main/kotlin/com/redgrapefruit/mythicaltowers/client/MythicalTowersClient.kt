fc39620dedd0f6e6e48536b8af122e437efcbc4c
RedGrapefruit <karpovanton729@gmail.com>
2021-05-28 17:24:09 +0300

    Fixed compile errors

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/client/MythicalTowersClient.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/client/MythicalTowersClient.kt
@@ -1,13 +1,10 @@
 package com.redgrapefruit.mythicaltowers.client
 
-import com.redgrapefruit.mythicaltowers.client.init.MythicalEntityRenderers
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 
 @Environment(EnvType.CLIENT)
 class MythicalTowersClient : ClientModInitializer {
-    override fun onInitializeClient() {
-        MythicalEntityRenderers.init()
-    }
+    override fun onInitializeClient() {}
 }
