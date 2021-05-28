4edf9f81b27b5b3ab1934b4ba69a1239aee0ca65
RedGrapefruit <karpovanton729@gmail.com>
2021-05-28 17:19:11 +0300

    Refactoring & regions: ContainerBlock.kt

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/api/ContainerBlock.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/api/ContainerBlock.kt
@@ -20,15 +20,30 @@ import net.minecraft.util.math.Direction
 import net.minecraft.world.World
 
 /**
- * A container block synced with a [ContainerBlockEntity].<br></br>
- * Opens a [Screen] & a [ScreenHandler] when right-clicked.<br></br>
- * Also acts as a directional block.<br></br><br></br>
- * A part of RedCore.Container library bundled with this mod.
+ * A container block synced with a [ContainerBlockEntity].
+ * Opens a [Screen] & a [ScreenHandler] when right-clicked.
+ *
+ * Also acts as a directional block.
  */
 @Suppress("DEPRECATION")
 abstract class ContainerBlock protected constructor(settings: Settings?) : BlockWithEntity(settings) {
-    private var facing // Facing property
-            : DirectionProperty? = null
+    // region Properties & Constructor
+
+    private lateinit var facing // Facing property
+            : DirectionProperty
+
+    init {
+        // Defaults facing property
+        defaultState =
+            getStateManager().defaultState.with(
+                facing,
+                Direction.NORTH
+            )
+    }
+
+    // endregion
+
+    // region Abstract
 
     /**
      * Checks if given [BlockEntity] is instanceof current [BlockEntity]
@@ -44,28 +59,26 @@ abstract class ContainerBlock protected constructor(settings: Settings?) : Block
      * @param blockEntity Current block entity
      * @return Casted to inventory
      */
-    protected abstract fun castToInventory(blockEntity: BlockEntity?): Inventory?
+    protected abstract fun castToInventory(blockEntity: BlockEntity?): Inventory
+
+    // endregion
+
+    // region Directional features
+
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
         facing = Properties.HORIZONTAL_FACING
         builder.add(facing)
     }
 
-    override fun rotate(state: BlockState, rotation: BlockRotation): BlockState {
-        return state.with(facing, rotation.rotate(state.get(facing)))
-    }
+    override fun rotate(state: BlockState, rotation: BlockRotation): BlockState = state.with(facing, rotation.rotate(state.get(facing)))
 
-    override fun mirror(state: BlockState, mirror: BlockMirror): BlockState {
-        return state.rotate(mirror.getRotation(state.get(facing)))
-    }
+    override fun mirror(state: BlockState, mirror: BlockMirror): BlockState = state.rotate(mirror.getRotation(state.get(facing)))
 
-    override fun getPlacementState(context: ItemPlacementContext): BlockState? {
-        return defaultState.with(facing, context.playerFacing.opposite)
-    }
+    override fun getPlacementState(context: ItemPlacementContext): BlockState = defaultState.with(facing, context.playerFacing.opposite)
 
-    override fun getRenderType(state: BlockState): BlockRenderType {
-        // BlockWithEntity resets render type to INVISIBLE, so we deny it
-        return BlockRenderType.MODEL
-    }
+    // endregion
+
+    // region Screen(Handler) management
 
     override fun onUse(
         state: BlockState,
@@ -100,20 +113,16 @@ abstract class ContainerBlock protected constructor(settings: Settings?) : Block
         }
     }
 
-    override fun hasComparatorOutput(state: BlockState): Boolean {
-        return true
-    }
+    // endregion
 
-    override fun getComparatorOutput(state: BlockState, world: World, pos: BlockPos): Int {
-        return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos))
-    }
+    // region Misc
 
-    init {
-        // Defaults facing property
-        defaultState =
-            getStateManager().defaultState.with(
-                facing,
-                Direction.NORTH
-            )
-    }
+    override fun hasComparatorOutput(state: BlockState): Boolean = true
+
+    override fun getComparatorOutput(state: BlockState, world: World, pos: BlockPos): Int = ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos))
+
+    // BlockWithEntity resets BlockRenderType to INVISIBLE, so we set it back to MODEL
+    override fun getRenderType(state: BlockState): BlockRenderType = BlockRenderType.MODEL
+
+    // endregion
 }
