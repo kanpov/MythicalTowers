a8a0216ff2064b0dbf8f7888cd7220d70184e6a5
RedGrapefruit <karpovanton729@gmail.com>
2021-05-29 10:30:44 +0300

    Fixed crash due to "unregister-able" EntityRenderer

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/client/MythicalTowersClient.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/client/MythicalTowersClient.kt
@@ -1,10 +1,19 @@
 package com.redgrapefruit.mythicaltowers.client
 
+import com.redgrapefruit.mythicaltowers.client.render.entity.GreenTntEntityRenderer
+import com.redgrapefruit.mythicaltowers.common.init.MythicalEntities
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry
+import net.minecraft.client.render.entity.EntityRenderDispatcher
 
 @Environment(EnvType.CLIENT)
 class MythicalTowersClient : ClientModInitializer {
-    override fun onInitializeClient() {}
+    override fun onInitializeClient() {
+        // Temp solution: entity renderer registry doesn't work. This's a placeholder
+        EntityRendererRegistry.INSTANCE.register(MythicalEntities.GREEN_TNT) {
+            dispatcher: EntityRenderDispatcher, context: EntityRendererRegistry.Context -> GreenTntEntityRenderer(dispatcher)
+        }
+    }
 }
