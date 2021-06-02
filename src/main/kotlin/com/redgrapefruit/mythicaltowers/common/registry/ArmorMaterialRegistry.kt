2a8d46163cfefb389acdfcfdf44a848cf2a20251
RedGrapefruit <karpovanton729@gmail.com>
2021-06-02 16:07:57 +0300

    Setup all necessary ArmorMaterials for later

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/common/registry/ArmorMaterialRegistry.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/common/registry/ArmorMaterialRegistry.kt
@@ -23,13 +23,84 @@ enum class ArmorMaterialRegistry(
 ) : ArmorMaterial {
 
     GREEN(
-        intArrayOf(300, 350, 400, 350), intArrayOf(4, 5, 6, 5),
+        intArrayOf(300, 350, 400, 350),
+        intArrayOf(4, 5, 6, 5),
         17,
         SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
         ItemRegistry.GREEN_INGOT,
         "green",
         4.0f,
         0.2f
+    ),
+    YELLOW(
+        intArrayOf(600, 650, 700, 650),
+        intArrayOf(7, 8, 9, 8),
+        20,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        ItemRegistry.YELLOW_INGOT,
+        "yellow",
+        6.0f,
+        0.3f
+    ),
+    ORANGE(
+        intArrayOf(900, 950, 1000, 950),
+        intArrayOf(10, 11, 12, 11),
+        23,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        ItemRegistry.ORANGE_INGOT,
+        "orange",
+        8.0f,
+        0.4f
+    ),
+    RED(
+        intArrayOf(1200, 1250, 1300, 1250),
+        intArrayOf(13, 14, 15, 14),
+        26,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        ItemRegistry.RED_INGOT,
+        "red",
+        10.0f,
+        0.5f
+    ),
+    BLUE(
+        intArrayOf(1500, 1550, 1600, 1550),
+        intArrayOf(16, 17, 18, 17),
+        29,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        ItemRegistry.BLUE_INGOT,
+        "blue",
+        12.0f,
+        0.6f
+    ),
+    PURPLE(
+        intArrayOf(1800, 1850, 1900, 1850),
+        intArrayOf(19, 20, 21, 20),
+        32,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        ItemRegistry.PURPLE_INGOT,
+        "purple",
+        14.0f,
+        0.7f
+    ),
+    GRAY(
+        intArrayOf(2100, 2150, 2200, 2150),
+        intArrayOf(22, 23, 24, 23),
+        35,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        ItemRegistry.GRAY_INGOT,
+        "gray",
+        16.0f,
+        0.8f
+    ),
+    BLACK(
+        intArrayOf(2400, 2450, 2500, 2450),
+        intArrayOf(25, 26, 27, 26),
+        38,
+        SoundEvents.ITEM_ARMOR_EQUIP_GENERIC,
+        ItemRegistry.BLACK_INGOT,
+        "black",
+        18.0f,
+        0.9f
     );
 
     private val repairIngredient: Ingredient = Ingredient.ofItems(repairIngredient)
