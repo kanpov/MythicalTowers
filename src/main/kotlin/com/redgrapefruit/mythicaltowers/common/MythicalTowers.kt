bc4f2049d5c3abcc80d73fafb1129e01afc85626
RedGrapefruit <karpovanton729@gmail.com>
2021-06-17 14:43:42 +0300

    Add pure material blocks

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/common/MythicalTowers.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/common/MythicalTowers.kt
@@ -5,6 +5,9 @@ import com.redgrapefruit.mythicaltowers.common.registry.EntityRegistry
 import com.redgrapefruit.mythicaltowers.common.registry.ItemGroupRegistry
 import com.redgrapefruit.mythicaltowers.common.registry.ItemRegistry
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags
+import net.minecraft.block.Material
 import net.minecraft.util.Identifier
 import java.util.*
 
@@ -27,6 +30,20 @@ class MythicalTowers : ModInitializer {
          */
         fun idOf(name: String): Identifier {
             return Identifier("mythicaltowers", name)
+        }
+
+        /**
+         * Creates standard block settings
+         *
+         * @param hardness Block's hardness
+         */
+        fun blockSettings(hardness: Float): FabricBlockSettings {
+            return FabricBlockSettings
+                .of(Material.METAL)
+                .collidable(true)
+                .breakByHand(false)
+                .breakByTool(FabricToolTags.PICKAXES, 4)
+                .hardness(hardness)
         }
     }
 }
--- src/main/kotlin/com/redgrapefruit/mythicaltowers/common/registry/BlockRegistry.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/common/registry/BlockRegistry.kt
@@ -1,12 +1,12 @@
 package com.redgrapefruit.mythicaltowers.common.registry
 
+import com.redgrapefruit.mythicaltowers.common.MythicalTowers.Companion.blockSettings
 import com.redgrapefruit.mythicaltowers.common.MythicalTowers.Companion.idOf
+import com.redgrapefruit.mythicaltowers.common.block.building.PureMaterialBlock
 import com.redgrapefruit.mythicaltowers.common.block.trap.FireStationBlock
 import com.redgrapefruit.mythicaltowers.common.block.trap.GreenTntBlock
 import com.redgrapefruit.mythicaltowers.common.block.trap.JumpPadBlock
-import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
-import net.minecraft.block.Material
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
@@ -17,14 +17,34 @@ import net.minecraft.util.registry.Registry
  */
 @Suppress("MemberVisibilityCanBePrivate")
 object BlockRegistry {
+
+    // Building
+    val PURE_GREEN_BLOCK = PureMaterialBlock(blockSettings(3.0f))
+    val PURE_YELLOW_BLOCK = PureMaterialBlock(blockSettings(4.5f))
+    val PURE_ORANGE_BLOCK = PureMaterialBlock(blockSettings(6.0f))
+    val PURE_RED_BLOCK = PureMaterialBlock(blockSettings(7.5f))
+    val PURE_BLUE_BLOCK = PureMaterialBlock(blockSettings(9.0f))
+    val PURE_PURPLE_BLOCK = PureMaterialBlock(blockSettings(10.5f))
+    val PURE_GRAY_BLOCK = PureMaterialBlock(blockSettings(12.0f))
+    val PURE_BLACK_BLOCK = PureMaterialBlock(blockSettings(13.5f))
+
     // Traps
     val GREEN_TNT = GreenTntBlock()
 
     val GREEN_JUMP_PAD = JumpPadBlock(1.5, 1.1)
 
-    val GREEN_FIRE_STATION = FireStationBlock(FabricBlockSettings.of(Material.METAL).hardness(2.0f))
+    val GREEN_FIRE_STATION = FireStationBlock(blockSettings(2.0f))
 
     fun init() {
+        register("pure_green_block", PURE_GREEN_BLOCK, ItemGroupRegistry.BUILDING)
+        register("pure_yellow_block", PURE_YELLOW_BLOCK, ItemGroupRegistry.BUILDING)
+        register("pure_orange_block", PURE_ORANGE_BLOCK, ItemGroupRegistry.BUILDING)
+        register("pure_red_block", PURE_RED_BLOCK, ItemGroupRegistry.BUILDING)
+        register("pure_blue_block", PURE_BLUE_BLOCK, ItemGroupRegistry.BUILDING)
+        register("pure_purple_block", PURE_PURPLE_BLOCK, ItemGroupRegistry.BUILDING)
+        register("pure_gray_block", PURE_GRAY_BLOCK, ItemGroupRegistry.BUILDING)
+        register("pure_black_block", PURE_BLACK_BLOCK, ItemGroupRegistry.BUILDING)
+
         register("green_tnt", GREEN_TNT, ItemGroupRegistry.TRAPS)
 
         register("green_jump_pad", GREEN_JUMP_PAD, ItemGroupRegistry.TRAPS)
