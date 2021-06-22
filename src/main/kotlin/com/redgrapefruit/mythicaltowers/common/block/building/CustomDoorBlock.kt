f71e633fc5a38c576b14c3f60ebef619834c7edd
RedGrapefruit <karpovanton729@gmail.com>
2021-06-22 09:43:34 +0300

    Create the door logic

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/common/block/building/CustomDoorBlock.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/common/block/building/CustomDoorBlock.kt
@@ -1,8 +1,37 @@
 package com.redgrapefruit.mythicaltowers.common.block.building
 
-import net.minecraft.block.DoorBlock
+import com.redgrapefruit.mythicaltowers.common.item.KeyItem
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
 
 /**
- * Custom door
+ * Custom door that requires a [KeyItem] to open.
  */
-class CustomDoorBlock(settings: Settings) : DoorBlock(settings)
+class CustomDoorBlock(private val key: KeyItem, settings: Settings) : Block(settings) {
+    override fun onUse(
+        state: BlockState,
+        world: World,
+        pos: BlockPos,
+        player: PlayerEntity,
+        hand: Hand,
+        hit: BlockHitResult
+    ): ActionResult {
+        val stack = player.getStackInHand(hand)
+
+        // Check if the door has been right-clicked with the linked key
+        if (stack.item == key) {
+            // Remove the door
+            world.removeBlock(pos, false)
+            // Decrement the stack with the key because a key can only be used once
+            stack.decrement(1)
+        }
+
+        return ActionResult.SUCCESS
+    }
+}
