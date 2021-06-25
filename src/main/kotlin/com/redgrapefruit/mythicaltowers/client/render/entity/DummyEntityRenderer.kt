b5c8d51ba57c574060ea189bd9f1e1e23e7a1ec4
RedGrapefruit <karpovanton729@gmail.com>
2021-06-25 16:21:40 +0300

    Get rid of @Deprecated at all costs!

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/client/render/entity/DummyEntityRenderer.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/client/render/entity/DummyEntityRenderer.kt
@@ -2,8 +2,8 @@ package com.redgrapefruit.mythicaltowers.client.render.entity
 
 import net.minecraft.client.render.entity.EntityRenderer
 import net.minecraft.client.render.entity.EntityRendererFactory
-import net.minecraft.client.texture.SpriteAtlasTexture
 import net.minecraft.entity.Entity
+import net.minecraft.screen.PlayerScreenHandler
 import net.minecraft.util.Identifier
 
 /**
@@ -11,5 +11,5 @@ import net.minecraft.util.Identifier
  */
 class DummyEntityRenderer<TEntity>(context: EntityRendererFactory.Context) :
     EntityRenderer<TEntity>(context) where TEntity : Entity {
-    override fun getTexture(entity: TEntity): Identifier = SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE
+    override fun getTexture(entity: TEntity): Identifier = PlayerScreenHandler.BLOCK_ATLAS_TEXTURE
 }
