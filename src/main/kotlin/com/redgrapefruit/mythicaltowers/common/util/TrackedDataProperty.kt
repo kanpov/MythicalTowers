8cfadd4471133e2823d7a1c0480d2f6c2a3f3312
RedGrapefruit <karpovanton729@gmail.com>
2021-07-03 14:51:29 +0300

    Remove unnecessary operations

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/common/util/TrackedDataProperty.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/common/util/TrackedDataProperty.kt
@@ -4,7 +4,6 @@ import net.minecraft.entity.Entity
 import net.minecraft.entity.data.DataTracker
 import net.minecraft.entity.data.TrackedData
 import net.minecraft.entity.data.TrackedDataHandler
-import net.minecraft.nbt.NbtCompound
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -41,33 +40,9 @@ class TrackedDataProperty<TOwner, TSelf>(
     private val tracker: DataTracker,
     clazz: Class<TOwner>,
     handler: TrackedDataHandler<TSelf>,
-    private val operable: NbtOperable<TSelf>,
     private val data: TrackedData<TSelf> = DataTracker.registerData(clazz, handler)
 
 ) : ReadOnlyProperty<TOwner, TSelf> where TOwner : Entity {
-
-    /**
-     * Registers the property with given default value.
-     *
-     * Should only be called in `initDataTracker` overrides
-     */
-    fun register(default: TSelf) {
-        tracker.startTracking(data, default)
-    }
-
-    /**
-     * Reads this property from NBT via the [operable]
-     */
-    fun readNbt(nbt: NbtCompound, key: String) {
-        tracker.set(data, operable.read(nbt, key))
-    }
-
-    /**
-     * Writes this property to NBT via the [operable]
-     */
-    fun writeNbt(nbt: NbtCompound, key: String) {
-        operable.write(nbt, key, tracker.get(data))
-    }
 
     override fun getValue(thisRef: TOwner, property: KProperty<*>): TSelf {
         return tracker.get(data)
@@ -78,6 +53,6 @@ class TrackedDataProperty<TOwner, TSelf>(
  * A convenience method for creating [TrackedDataProperty]s that is inline to provide the Java [Class] automatically
  * through a reified type parameter.
  */
-inline fun <reified TOwner, TSelf> trackedData(tracker: DataTracker, handler: TrackedDataHandler<TSelf>, operable: NbtOperable<TSelf>): TrackedDataProperty<TOwner, TSelf> where TOwner : Entity {
-    return TrackedDataProperty(tracker, TOwner::class.java, handler, operable)
+inline fun <reified TOwner, TSelf> trackedData(tracker: DataTracker, handler: TrackedDataHandler<TSelf>): TrackedDataProperty<TOwner, TSelf> where TOwner : Entity {
+    return TrackedDataProperty(tracker, TOwner::class.java, handler)
 }
