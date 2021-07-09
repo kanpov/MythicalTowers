689b5cc520b3fd444855fd3b1f0f7dcd008fda04
RedGrapefruit <karpovanton729@gmail.com>
2021-07-09 10:45:36 +0300

    Fix environment issue

--- src/main/kotlin/com/redgrapefruit/mythicaltowers/client/MythicalTowersClient.kt
+++ src/main/kotlin/com/redgrapefruit/mythicaltowers/client/MythicalTowersClient.kt
@@ -5,10 +5,17 @@ import com.redgrapefruit.mythicaltowers.client.registry.EntityRendererRegistry
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
+import net.minecraft.text.LiteralText
 
 @Environment(EnvType.CLIENT)
 class MythicalTowersClient : ClientModInitializer {
     override fun onInitializeClient() {
+        // Thank the player for using the MythicalTowers mod in the chat
+        ClientPlayConnectionEvents.JOIN.register { _, _, client ->
+            client.player?.sendMessage(LiteralText("Thank you for using the MythicalTowers mod!"), false)
+        }
+
         EntityRendererRegistry.init()
         EntityModelLayerRegistry.init()
     }
